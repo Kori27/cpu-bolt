f0bf6f8e20fa132fe30299bce6495b071a5c1252
Sz Kori <kori27@gmail.com>
2023-04-26 14:02:42 +0200

    ejs beállítása

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const app = express;
+const app = express();
 
 const PORT = process.env.PORT || 3500;
 
-app.length('/', (req,res) => {
-    try {
-        res.status(200).json({ msg : 'Sikeres elindítás!'});
-    }
-    res 
-    }
-})
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  try {
+    res.status(200).json({ msg: 'Sikeres elindítás!' });
+  } catch (error) {
+    res.status(500).json({ msg: 'Valami elromlott!' });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('http://localhost:${PORT}');
-})
+  console.log(`http://localhost:${PORT}`);
+});
